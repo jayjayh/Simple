e37eb06afa22701490e66c4babf291cb9bd4fb47
andrew-qu <33229508+andrew-qu@users.noreply.github.com>
2021-10-26 23:20:21 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -91,7 +91,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors:{
-		origin: "http://localhost:3000",
+		origin: "https://simplefornow.herokuapp.com",
 		methods:["GET","POST"]
 	},
 	
